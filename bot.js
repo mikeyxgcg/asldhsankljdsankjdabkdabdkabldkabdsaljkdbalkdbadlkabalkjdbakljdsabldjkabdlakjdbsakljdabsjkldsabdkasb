a3e5b179d95904530fa75376b4196a12339c85ab
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-08-12 23:18:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -425,42 +425,6 @@ client.on('message', message => {
 
 
 
-Client.on('message', message => {
-if (message.content.split(' ')[0] == '-bc')
- message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
-                                                            message.delete();
-});
-});
-Client.on("message", message => {
-    var prefix = "-";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith("-bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
- if (!args[1]) {
-                                let embed3 = new Discord.RichEmbed()
-                                .setDescription(":white_check_mark: | تم ارسال للكل رساله فارغه")
-                                .setColor("#FF0000")
-                                message.channel.sendEmbed(embed3);
-                            } else {
-
-                            let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال للكل الرساله !')
-                                                            .setColor("#008000")
-                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            }
-                          }
-                         
-});
-
-
-
-
-
-
 
 
 
