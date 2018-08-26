a6532aedc867b964292a7433945651498073e91d
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-08-26 09:08:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -534,6 +534,61 @@ client.on('message',  (message) => {
 });
 
 
+client.on('message',  (message) => {
+        if(message.content.startsWith('-love')) {
+  let user = message.mentions.users.first();
+  if (!user) {
+
+    return message.emit('commandUsage', message, this.help);
+  }
+  let love = [
+    'https://media.giphy.com/media/YDB4EF3U6i6IM/giphy.gif',
+    'https://media.giphy.com/media/l41JWw65TcBGjPpRK/giphy.gif',
+    'https://media.giphy.com/media/3o6gDZ9unSrDk3EuR2/giphy.gif',
+    'https://media.giphy.com/media/ymkLJAxVz2la/giphy.gif',
+    'https://media.giphy.com/media/ZOln4JxCoZay4/giphy.gif',
+    'https://media.giphy.com/media/l0K4kWJir91VEoa1W/giphy.gif',
+    'https://media.giphy.com/media/X3FmqQ7ehoCBy/giphy.gif',
+    'https://media.giphy.com/media/VlzUkJJzvz0UU/giphy.gif',
+    'https://media.giphy.com/media/NbPJFUS6Vkx7q/giphy.gif',
+    'https://media.giphy.com/media/wDEWXcisSjrQQ/giphy.gif',
+    'https://media.giphy.com/media/xT8qBuhwq0OyL7UkdW/giphy.gif',
+    'https://media.giphy.com/media/gfvxlwRKH1y5q/giphy.gif',
+    'https://media.giphy.com/media/PuTSgeacS3Z7i/giphy.gif',
+    'https://media.giphy.com/media/l49JBwneyflgjm5zO/giphy.gif',
+    'https://media.giphy.com/media/QKUA2bIAgjFgk/giphy.gif',
+    'https://media.giphy.com/media/T3Uzzre7Ap0mk/giphy.gif',
+    'https://media.giphy.com/media/3oeSB6pawq6G99xCXS/giphy.gif',
+    'https://media.giphy.com/media/iKIgD5j0I3hMQ/giphy.gif',
+    'https://media.giphy.com/media/hhHcFH0xAduCs/giphy.gif',
+    'https://media.giphy.com/media/3o7qDJKIO5rSeyHhvO/giphy.gif',
+    'https://media.giphy.com/media/92bGINsmjAuUE/giphy.gif',
+    'https://media.giphy.com/media/bErElGdAHUmoE/giphy.gif',
+    'https://media.giphy.com/media/jQqU9dCKUOdri/giphy.gif',
+    'https://media.giphy.com/media/10uJ0IFxlCA06I/giphy.gif',
+    'https://media.giphy.com/media/bMLGNRoAy0Yko/giphy.gif',
+    'https://media.giphy.com/media/3osxYcry2fDfqyhOQ8/giphy.gif',
+    'https://media.giphy.com/media/3ohs84a6AyArTscVsk/giphy.gif',
+    'https://media.giphy.com/media/3o6Zt6D0wctP0kpuwg/giphy.gif',
+    'https://media.giphy.com/media/4zmFt0xpke8AU/giphy.gif',
+    'https://media.giphy.com/media/l3vR9O3vpOCDRo8rC/giphy.gif',
+    'https://media.giphy.com/media/13sgibUDaaaEU0/giphy.gif'
+  ];
+
+  message.channel.send({
+    embed: {
+      description: `${message.author.username} :heart:  يعبر لك عن حبه الحقيقي   ${user.username}!`,
+      image: {
+        url: loves[Math.floor(Math.random() * loves.length)]
+      }
+    }
+  }).catch(e => {
+    client.log.error(e);
+  })
+        }  
+});
+
+
    var prefix = "-";
 var cats = [
 
