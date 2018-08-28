b4523be83855b4512b83e268bfd412d564b0be3d
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-08-28 08:57:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -973,6 +973,18 @@ client.on('message',async message => {
   }
 });
 
+var prefix = "-"
+
+client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , " تــــم ارســالك في الخــاص")
+  message.channel.sendEmbed(embed);
+    }
+});
+
 
 client.on("message", message => {
     var prefix = "-";
