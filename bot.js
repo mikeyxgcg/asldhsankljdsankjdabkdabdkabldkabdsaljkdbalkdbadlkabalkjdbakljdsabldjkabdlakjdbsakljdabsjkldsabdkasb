2d8a7e6ff72c87ee646bd001cdde34357f9dda22
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-09-01 21:50:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1405,6 +1405,20 @@ message.channel.sendFile(canvas.toBuffer())
  
 });
 
+client.on('message', async msg => {
+     client.snek = require('snekfetch');
+    var p = "-"
+  if(msg.content.startsWith(p + "tallk")) {
+   let args = msg.content.split(' ').slice(1).join(' ');
+
+ if(args.length < 1) return args.missing(msg, 'No text added', this.help);
+  msg.channel.startTyping();
+  const searchMessage = await msg.channel.send('🖌️Painting...');
+  const { body } = await client.snek.get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${encodeURIComponent(args)}`);
+  msg.channel.send({file: { attachment:body.message, name: 'changemymind.png'}}).then(()=> { searchMessage.delete(); msg.channel.stopTyping(); });
+};
+});
+
 
 client.on('message', message => {
   if (message.author.bot) return;
