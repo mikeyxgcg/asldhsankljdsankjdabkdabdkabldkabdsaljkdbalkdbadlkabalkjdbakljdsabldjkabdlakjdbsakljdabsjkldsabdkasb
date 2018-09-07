c8a7bf3154a8ac8f996ee8b8794dd70440e8cf97
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-09-07 20:07:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -350,6 +350,34 @@ client.on("message", message => {
  }
 }); 
 
+client.on('message',message =>{
+    var prefix = "-";
+    if(message.content.startsWith(prefix + 'top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://cdn.discordapp.com/attachments/462220624656334849/487684860442443787/ce5fb05919818916b5f598f3ee18afaa.png")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
 client.on("message", message => {
  if (message.content === "-support") {
   const embed = new Discord.RichEmbed()
