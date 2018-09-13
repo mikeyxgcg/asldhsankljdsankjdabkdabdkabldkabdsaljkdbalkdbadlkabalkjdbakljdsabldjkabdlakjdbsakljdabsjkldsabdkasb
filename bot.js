300c120d2770b6922c0385b089c6c033c1475ad4
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-09-13 08:51:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('ready', () => {
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = [`${client.guilds.size} Server`,' Premium Bot™ by n3k4a & Baron','Type -help',`${client.users.size} Members`,'-invite','By: n3k4a & Hosam | BaronTube'];
+    var setGame = [`${client.guilds.size} Server`,' Premium Bot™ by n3k4a & Baron','Type -help |-support |-invite ',`${client.users.size} Members`,'-invite','By: n3k4a & Hosam | BaronTube'];
     var i = -1;
     var j = 0;
     setInterval(function (){
@@ -1133,6 +1133,7 @@ client.on('message',  (message) => {
 
 
    var prefix = "-";
+
 var cats = [
 
 "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
