457607aae4c14ad1db6fcb6862e36652a65c1b3d
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-09-13 10:47:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -388,17 +388,16 @@ client.on("guildMemberAdd", member => {
       }
       });
 
-var fs = require("fs");
-client.on("guildMemberAdd", member => {
-member.guild.fetchInvites().then(guildInvites => {
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const yumz = member.guild.channels.find("name", `${sChannel}`);
      yumz.send(`<@${member.user.id}> joined by <@${inviter.id}>`);
-   //  yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  })
-    });
+     yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+});
 
 client.on('message', async message =>{
   if (message.author.boss) return;
