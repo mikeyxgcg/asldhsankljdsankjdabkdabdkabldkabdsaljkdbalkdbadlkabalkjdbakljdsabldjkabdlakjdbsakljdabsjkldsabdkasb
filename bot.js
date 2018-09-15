42db67e8e42984867882cd2db37a0dff536c41cf
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-09-15 23:46:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2154,9 +2154,7 @@ if (message.content.startsWith('-server')) {
             ]
         }
     })
-
-
-  var prefix = "-";
+var prefix = "-";
     client.on('message', message => {
     if(message.content.startsWith(prefix + '2avatar')) {
          var men = message.mentions.users.first();
